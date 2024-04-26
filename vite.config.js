7a7053b9aa9252915b2e68c218e821888e2068f1
kwsni <44220303+kwsni@users.noreply.github.com>
2024-04-26 05:04:23 -0700

    Fixed .gitignore

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+import tailwindcss from 'tailwindcss'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      external: [
+        fileURLToPath(
+          new URL(
+            'src/external/headless.js',
+            import.meta.url
+          )
+        )
+      ]
+    }
+  }
+})
