831aa063618e223b60250cef7c36b7844f13bdab
kwsni <44220303+kwsni@users.noreply.github.com>
2025-02-03 23:50:03 -0800

    Fixed dates in API

--- api/app.py
+++ api/app.py
@@ -5,22 +5,24 @@ from flask import Flask, request, send_file
 
 load_dotenv()
 
-dtToday = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-dtWeek = dtToday + datetime.timedelta(days=7)
+def dtToday():
+    return datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+def dtWeek():
+    return dtToday + datetime.timedelta(days=7)
 
 app = Flask(__name__)
 
 @app.route('/api/sonarrCal', methods=['GET'])
 def get_sonarr_cal():
     if request.method == 'GET':
-        data = requests.get(f'{os.getenv("VITE_SONARR_URL")}/api/v3/calendar?start={dtToday.isoformat()}&end={dtWeek.isoformat()}&includeSeries=true&apikey={os.getenv("VITE_SONARR_API_KEY")}')
+        data = requests.get(f'{os.getenv("VITE_SONARR_URL")}/api/v3/calendar?start={dtToday().isoformat()}&end={dtWeek().isoformat()}&includeSeries=true&apikey={os.getenv("VITE_SONARR_API_KEY")}')
         return data.content, data.status_code
 
 
 @app.route('/api/tautulliWatched', methods=['GET'])
 def get_tautulli_watched():
     if request.method == 'GET':
-        data = requests.get(f'{os.getenv("VITE_TAUTULLI_URL")}/api/v2/?apikey={os.getenv("VITE_TAUTULLI_API_KEY")}&cmd=get_history&section_id=2&after={dtToday.isoformat()}')
+        data = requests.get(f'{os.getenv("VITE_TAUTULLI_URL")}/api/v2/?apikey={os.getenv("VITE_TAUTULLI_API_KEY")}&cmd=get_history&section_id=2&after={dtToday().isoformat()}')
         return data.content, data.status_code
         
 @app.route('/api/posters/<seriesId>', methods=['GET'])
